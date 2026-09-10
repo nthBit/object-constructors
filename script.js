66230b5780c1c0e7a3ed116d29bd5555dd393e0c
nthBit <207577365+nthBit@users.noreply.github.com>
2026-09-10 12:20:41 +0100

    Finish basic script and start on form

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, hasRead) {
     this.title = title;
     this.author = author;
@@ -6,7 +8,33 @@ function Book(title, author, pages, hasRead) {
     this.info = function() {
         return this.title;
     }
+    this.id = crypto.randomUUID()
 };
 
-const hyperion = new Book('Hyperion', 'Dan Simmons', 480, false);
-console.log(hyperion.info());
+function createBook(title, author, pages, hasRead) {
+    let newBook = new Book(title, author, pages, hasRead);
+    myLibrary.push(newBook);
+};
+
+createBook('Hyperion', 'Dan Simmons', '480', 'false');
+createBook('The Fall of Hyperion', 'Dan Simmons', '517', 'false');
+createBook('Endymion', 'Dan Simmons', '441', 'false');
+createBook('The Rise of Endymion', 'Dan Simmons', '579', 'false');
+
+function arrayLoop() {
+    let table = "<table>";
+    myLibrary.forEach((book) => {
+        table += "<tr>"
+        table += `<td>${book.title}</td>`;
+        table += `<td>${book.author}</td>`;
+        table += `<td>${book.pages}</td>`;
+        table += `<td>${book.hasRead}</td>`;
+        table += `<td>${book.id}</td>`;
+        table += "</tr>"
+    });
+    table += "</table>"
+    return table;
+};
+
+const body = document.querySelector("body");
+body.innerHTML = arrayLoop();
