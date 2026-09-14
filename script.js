bd79d843a95a199360235e9846c567b48fed0daa
nthBit <207577365+nthBit@users.noreply.github.com>
2026-09-14 10:36:50 +0100

    Complete library logic

--- script.js
+++ script.js
@@ -8,23 +8,6 @@ function Book(title, author, pages, hasRead) {
     this.id = crypto.randomUUID();
 }
 
-// work on below function
-
-Book.prototype.bookRead = function() {
-    const hasReadBtns = Array.from(document.getElementsByClassName("hasReadBtn"));
-    hasReadBtns.forEach((button) => {
-        button.addEventListener("click", function(event){
-            event.preventDefault;
-            if (button.textContent === false) {
-                this.hasRead = true;
-            } else {
-                this.hasRead = false;
-            bookRead();
-        }
-        });
-    });
-}
-
 function createBook(title, author, pages, hasRead) {
     let newBook = new Book(title, author, pages, hasRead);
     myLibrary.push(newBook);
@@ -54,19 +37,6 @@ function arrayLoop() {
 const table = document.getElementById("table");
 table.innerHTML = arrayLoop();
 
-const submitButton = document.getElementById("submitBook");
-submitButton.addEventListener("submit", function(event) {
-    event.preventDefault();
-    const title = document.getElementById("title-input").value;
-    const author = document.getElementById("author-input").value;
-    const pages = document.getElementById("pages-input").value;
-    const hasRead = document.getElementById("hasRead-input").value;
-    createBook(title, author, pages, hasRead);
-    const table = document.getElementById("table");
-    table.innerHTML = arrayLoop();
-    deleteProcess();
-});
-
 function deleteProcess() {
     const deleteButtons = Array.from(document.getElementsByClassName("deleteButtons"));
     deleteButtons.forEach((button) => {
@@ -81,7 +51,38 @@ function deleteProcess() {
             const table = document.getElementById("table");
             table.innerHTML = arrayLoop();
             deleteProcess();
+            Book.prototype.bookRead();
         });
     });
 }
 deleteProcess();
+
+const submitButton = document.getElementById("submitBook");
+submitButton.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const title = document.getElementById("title-input").value;
+    const author = document.getElementById("author-input").value;
+    const pages = document.getElementById("pages-input").value;
+    const hasRead = document.getElementById("hasRead-input").value;
+    createBook(title, author, pages, hasRead);
+    const table = document.getElementById("table");
+    table.innerHTML = arrayLoop();
+    deleteProcess();
+    Book.prototype.bookRead();
+});
+
+Book.prototype.bookRead = function() {
+    const hasReadBtns = Array.from(document.getElementsByClassName("hasReadBtn"));
+    hasReadBtns.forEach((button, index) => {
+        button.addEventListener("click", function(event){
+            event.preventDefault();
+            if (button.textContent === "false") {
+                myLibrary[index].hasRead = "true";
+            } else {
+                myLibrary[index].hasRead = "false";
+            }
+            button.textContent = myLibrary[index].hasRead;
+        });
+    });
+}
+Book.prototype.bookRead();
